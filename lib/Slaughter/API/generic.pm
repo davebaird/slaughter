97b1eeae1f6d20eb91929d1773ba08447ef27a94
Steve Kemp <steve@steve.org.uk>
2014-10-23 08:32:34 +0100

    Updated POD.  Added version.

--- lib/Slaughter/API/generic.pm
+++ lib/Slaughter/API/generic.pm
@@ -17,23 +17,9 @@ without needing an OO-interface.
 
 =cut
 
+=head1 METHODS
 
-=head1 AUTHOR
-
- Steve
- --
- http://www.steve.org.uk/
-
-=cut
-
-=head1 LICENSE
-
-Copyright (c) 2010-2012 by Steve Kemp.  All rights reserved.
-
-This module is free software;
-you can redistribute it and/or modify it under
-the same terms as Perl itself.
-The LICENSE file contains the full text of the license.
+Now follows documentation on the available methods.
 
 =cut
 
@@ -61,14 +47,15 @@ use Text::Template;
 #
 use Slaughter::Private;
 
+#
+# The version of our release.
+#
+our $VERSION = "3.0.2";
 
-=begin doc
+
+=head2 import
 
 Export all subs in this package into the main namespace.
-
-This is nasty.
-
-=end doc
 
 =cut
 
@@ -1654,5 +1641,23 @@ sub UserCreate
 
 
 
-
 1;
+
+
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 LICENSE
+
+Copyright (c) 2010-2014 by Steve Kemp.  All rights reserved.
+
+This module is free software;
+you can redistribute it and/or modify it under
+the same terms as Perl itself.
+The LICENSE file contains the full text of the license.
+
+=cut
